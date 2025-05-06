86672de17eb2ddd2258859b4cfb4d943d9cedf28
davi <dcr23@inf.ufpr.br>
2025-05-06 08:55:12 -0300

    add script for feature importance graph

--- tools/graph_script/feature_importance.py
+++ tools/graph_script/feature_importance.py
@@ -1,13 +1,50 @@
 import sys
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
+# Load Data
+
 df = pd.read_csv(sys.argv[1])
+columns  = df.loc[0].sort_values(ascending=False).index
+df = df[columns]
 
-print(df.columns.values)
 
-print(df.iloc[1].values)
+# Plot
 
-plt.bar(df.columns.values,df.iloc[1].values)
+fig, ax = plt.subplots(figsize=(18,7))
 
-plt.show()
+bar_width = 0.4 
+
+label_locations = np.arange(len(df.columns.values))
+
+
+ax.axhline(y=0, color='black', linestyle='-', linewidth=1)
+
+ax.bar(label_locations, df.iloc[1].values, width=bar_width, edgecolor='black' , linewidth=2, orientation='vertical', label="Spearman")
+ax.bar(label_locations+bar_width, df.iloc[0].values, width=bar_width, edgecolor='black' , linewidth=2, orientation='vertical',label="Pearson")
+
+ax.set_xticks(label_locations+bar_width, df.columns.values)
+
+
+# Ticks
+
+plt.xticks(fontsize=16,rotation=90, style='italic')
+plt.yticks(ticks=np.arange(-1,1.1,0.2),fontsize=16)
+
+# Labels
+
+plt.title("Correlação de Pearson e Spearman entre Features e Labels", fontsize=20, weight='extra bold')
+plt.xlabel("Features", fontsize=18, weight='extra bold')
+
+plt.legend(loc='upper right', ncols=2, fontsize=12, alignment='center')
+
+# Grid 
+
+ax.grid(axis='y', ls='--')
+ax.set_axisbelow(True)
+
+# Savefig
+
+plt.savefig("pearson-feature-relation", edgecolor='black',bbox_inches='tight', pad_inches=0.5, dpi=300)
+
